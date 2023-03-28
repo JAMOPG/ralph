1b4a6f92de04175cc839df0eb099ff96bd84e50d
Quitterie Lucas <quitterielucas@outlook.fr>
2023-03-28 21:13:54 +0200

    ✅(tests) update cli tests
    As a new backend is implemented, cli help message for `ralph`  is modified.
    Tests have to be adapted.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -756,6 +756,12 @@ def test_cli_runserver_command_environment_file_generation(monkeypatch):
                 f"{settings.BACKENDS.DATABASE.CLICKHOUSE.PORT}\n",
                 "RALPH_BACKENDS__DATABASE__CLICKHOUSE__HOST="
                 f"{settings.BACKENDS.DATABASE.CLICKHOUSE.HOST}\n",
+                "RALPH_BACKENDS__DATABASE__CLICKHOUSE__CLIENT_OPTIONS"
+                "__date_time_input_format="
+                f"{settings.BACKENDS.DATABASE.CLICKHOUSE.CLIENT_OPTIONS.date_time_input_format}\n",  # noqa: E501 # pylint:disable=line-too-long
+                "RALPH_BACKENDS__DATABASE__CLICKHOUSE__CLIENT_OPTIONS"
+                "__allow_experimental_object_type="
+                f"{settings.BACKENDS.DATABASE.CLICKHOUSE.CLIENT_OPTIONS.allow_experimental_object_type}\n",  # noqa: E501 # pylint:disable=line-too-long
                 "RALPH_BACKENDS__DATABASE__MONGO__COLLECTION="
                 f"{settings.BACKENDS.DATABASE.MONGO.COLLECTION}\n",
                 "RALPH_BACKENDS__DATABASE__MONGO__DATABASE="
